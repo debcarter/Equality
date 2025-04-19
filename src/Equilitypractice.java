3af6c574f12a4390be138a1efc643b6e4fd6f0a2
Debora Carter <deborakcarter@gmail.com>
2025-04-19 00:40:19 -0600

    first commit

---
+++ src/Equilitypractice.java
@@ -0,0 +1,24 @@
+
+public class Equilitypractice {
+
+	public static void main(String[] args) {
+		
+		String a = new String("Goodbye");
+		String b = new String("Hello");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a == b);
+		System.out.println("equals() method: " + a.equals(b));
+		// Each Object has its own location in memory, so they different Objects even though they contain the same data.
+		// value of to Objects can be compared using the .equals() method.
+		int x = 5;
+		int y = 5;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(x == y);
+		//Primitive data types are comparing the same literal data; in this case 5 is the literal data.
+		
+	}
+
+}
